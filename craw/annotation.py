7c8a00902c2ecb7ecfefaf3963e6b3a89f7f52a6
Bertrand Néron <bneron@pasteur.fr>
2016-12-05 13:00:59 +0100

    allow several values for strand
    +/-  -1/+1 for/rev
    this commit fix the issue #14

--- craw/annotation.py
+++ craw/annotation.py
@@ -9,8 +9,13 @@ class Entry:
         :param values: the values parsed from one line of the annotation file
         :type values: list of string
         """
+        self._reverse_values = set(('-', '-1', 'rev'))
+        self._forward_values = set(('+', '1', 'for'))
+
         if len(values) != len(self._fields):
-            raise RuntimeError("the number of values does not match with number of fields")
+            raise RuntimeError("the number of values ({}) does not match with number of fields ({}): {}".format(len(values),
+                                                                                                            len(self._fields),
+                                                                                                                values))
         self._values = [self._convert(f, v) for f, v in zip(self._fields, values)]
         if self.start is not None and self.start > self.ref:
             raise RuntimeError(" error in line '{}': {} ({}) > {} ({})".format(self,
@@ -18,7 +23,7 @@ class Entry:
                                                                                self._fields_idx['start'].col_name,
                                                                                self.ref,
                                                                                self._fields_idx['ref'].col_name
-                                                                              ))
+                                                                               ))
         if self.stop is not None and self.stop < self.ref:
             raise RuntimeError(" error in line '{}': {} ({}) < {} ({})".format(self,
                                                                                self.stop,
@@ -30,8 +35,13 @@ class Entry:
         if field == self._fields_idx['ref'].col_name:
             value = int(value)
         elif field == self._fields_idx['strand'].col_name:
-            if value not in ('+', '-'):
-                raise RuntimeError("strand must be '+ or '-' got '{}'".format(value))
+            v = value.lower()
+            if v in self._forward_values:
+                value = '+'
+            elif v in self._reverse_values:
+                value = '-'
+            else:
+                raise RuntimeError("strand must be '+/-', '1/-1' or 'for/rev' got '{}'".format(value))
         elif 'start' in self._fields_idx and field == self._fields_idx['start'].col_name:
             value = int(value)
         elif 'stop' in self._fields_idx and field == self._fields_idx['stop'].col_name:
