f38af6fb30330b2ed6fc3a6d30f169cf707fe9bb
Bertrand Néron <bneron@pasteur.fr>
2017-02-02 14:27:23 +0100

    add lot of options to craw_htmp script
    and call the corresponding function with rigth argument from
    craw/heatmap

--- craw/heatmap.py
+++ craw/heatmap.py
@@ -1,9 +1,13 @@
 from inspect import isfunction
 import sys
+import logging
 
 import matplotlib.pyplot as plt
 import matplotlib as mtp
 import pandas as pd
+
+_log = logging.getLogger(__name__)
+_log.setLevel(logging.NOTSET)
 
 
 def get_data(coverage_file):
@@ -28,6 +32,8 @@ def split_data(data):
 
 
 def sort(data, criteria, **kwargs):
+    if data is None or data.empty:
+        return data
     func_name = '_sort_' + criteria
     all_func = globals()
     if func_name in all_func and isfunction(all_func[func_name]):
@@ -37,6 +43,7 @@ def sort(data, criteria, **kwargs):
         raise RuntimeError('BLABLA')
 
 def _sort_by_gene_size(data, start_col=None, stop_col=None):
+    _log.info("by gene size using cols {}:{}".format(start_col, stop_col))
     data['gene_len'] = abs(data[stop_col] - data[start_col])
     data = data.sort_values('gene_len', axis='index')
     del data['gene_len']
@@ -44,11 +51,13 @@ def _sort_by_gene_size(data, start_col=None, stop_col=None):
 
 
 def _sort_using_col(data, col=None):
+    _log.info("using col {}".format(col))
     data = data.sort_values(col, axis='index')
     return data
 
 
 def _sort_using_file(data, file=None):
+    _log.info("using file {}".format(file))
     ref = pd.read_table(file, comment="#")
     col_name = ref.columns[0]
 
@@ -61,6 +70,8 @@ def _sort_using_file(data, file=None):
 
 
 def crop_matrix(data, start_col, stop_col):
+    if data is None or data.emtpy:
+        return data
     return data.loc[:, start_col:stop_col]
 
 
@@ -70,12 +81,15 @@ def remove_metadata(data):
     :param data:
     :return:
     """
+    if data is None or data.empty:
+        return data
+
     def find_col_2_split(data):
         prev_col = None
         for col in data.columns:
             try:
                 int(col)
-            except:
+            except ValueError:
                 prev_col = col
                 continue
             else:
@@ -111,7 +125,7 @@ def draw_one_matrix(mat, ax, cmap=plt.cm.seismic, y_label=None):
     return mat_img
 
 
-def draw_heatmap(sense, antisense, color_map=plt.cm.Blues):
+def draw_heatmap(sense, antisense, color_map=plt.cm.Blues, title='', sense_on='top', norm=None):
     """
 
     :param sense:
@@ -123,43 +137,50 @@ def draw_heatmap(sense, antisense, color_map=plt.cm.Blues):
     draw_antisense = True
     if sense is None or sense.empty:
         draw_sense = False
-        print("there is n")
     if antisense is None or antisense.empty:
         draw_antisense = False
 
     if all((draw_sense, draw_antisense)):
-        print("all")
-        fig, axes_array = plt.subplots(nrows=2, ncols=1, figsize=(7, 10))
+        if sense_on in ('bottom', 'top'):
+            fig, axes_array = plt.subplots(nrows=2, ncols=1, figsize=(7, 10))
+            if sense_on == 'top':
+                sense_subplot, antisense_subplot = axes_array
+            else:
+                antisense_subplot, sense_subplot = axes_array
+        elif sense_on in ('left', 'right'):
+            fig, axes_array = plt.subplots(nrows=1, ncols=2, figsize=(10, 7))
+            if sense_on == 'left':
+                sense_subplot, antisense_subplot = axes_array
+            else:
+                antisense_subplot, sense_subplot = axes_array
+
     elif any((draw_sense, draw_antisense)):
-        print("any")
         fig, axes_array = plt.subplots(nrows=1, ncols=1, figsize=(7, 10))
     else:
-        print("WARNING: no matrix to draw", file=sys.stderr)
+        _log.warning("No matrix to draw")
         return
-    fig.suptitle("figure name", fontsize='large')
 
-    log_norm = mtp.colors.LogNorm()
+    fig.suptitle(title, fontsize='large')
+
 
     if draw_sense:
-        print("INFO: drawing sense matrix", file=sys.stderr)
-        sense_subplot = axes_array[0]
+        _log.info("Drawing sense matrix")
         sense_img = draw_one_matrix(sense, sense_subplot, cmap=color_map, y_label="Sense")
-        sense_img.set_norm(log_norm)
+        if norm:
+            sense_img.set_norm(norm)
     if draw_antisense:
-        print("INFO: drawing sense matrix", file=sys.stderr)
-        if draw_sense:
-            antisense_subplot = axes_array[1]
-        else:
-            antisense_subplot = axes_array[0]
+        _log.info("Drawing antisense matrix")
         antisense_img = draw_one_matrix(antisense, antisense_subplot, cmap=color_map, y_label="Anti sense")
-        antisense_img.set_norm(log_norm)
+        if norm:
+            antisense_img.set_norm(norm)
 
-    fig.suptitle('this is the figure title',
+    fig.suptitle(title,
                  fontsize=12,
                  horizontalalignment='center',
-                 verticalalignment='bottom')
+                 verticalalignment='top')
 
     fig.tight_layout()
-    fig.subplots_adjust(top=0.95)
-    fig.canvas.set_window_title('window name')
+    if sense_on in ('top', 'bottom'):
+        fig.subplots_adjust(top=0.95)
+    fig.canvas.set_window_title(title)
     return fig
