11b7a515f6fb327b0ab1f30e7ce4ffade505fc07
Bertrand Néron <bneron@pasteur.fr>
2017-03-02 17:05:03 +0100

    add module to parse wig files and store coverage along genome

---
+++ craw/wig.py
@@ -0,0 +1,305 @@
+###########################################################################
+#                                                                         #
+# This file is part of Counter RNAseq Window (craw) package.              #
+#                                                                         #
+#    Authors: Bertrand Néron                                              #
+#    Copyright © 2017  Institut Pasteur (Paris).                          #
+#    see COPYRIGHT file for details.                                      #
+#                                                                         #
+#    craw is free software: you can redistribute it and/or modify         #
+#    it under the terms of the GNU General Public License as published by #
+#    the Free Software Foundation, either version 3 of the License, or    #
+#    (at your option) any later version.                                  #
+#                                                                         #
+#    craw is distributed in the hope that it will be useful,              #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 #
+#    See the GNU General Public License for more details.                 #
+#                                                                         #
+#    You should have received a copy of the GNU General Public License    #
+#    along with craw (see COPYING file).                                  #
+#    If not, see <http://www.gnu.org/licenses/>.                          #
+#                                                                         #
+###########################################################################
+
+import re
+from abc import ABCMeta, abstractmethod
+
+
+class WigException(Exception):
+    pass
+
+
+class ExpandedStrand:
+
+    def __init__(self, start, stop, span):
+        """
+
+        :param start: start position of the chunk (1 based position)
+        :param stop: stop position of the chunk (1 based position)
+        :param span: length of span
+        """
+        self._start = start
+        # be careful the last position count in the span
+        self.coverage = [0] * (stop + span - start)
+
+    def __getattr__(self, item):
+        return getattr(self.coverage, item)
+
+    def __len__(self):
+        return len(self.coverage)
+
+    @property
+    def stop(self):
+        return len(self.coverage) + self._start - 1
+
+    @property
+    def start(self):
+        return self._start
+
+    def __eq__(self, other):
+        return self._start == other._start and self.coverage == other.coverage
+
+    def __setitem__(self, idx, value):
+        print("\n@@@@@@@@@ idx",idx)
+        print("@@@@@@@@ len coverage", len(self.coverage))
+        self.coverage[idx] = value
+
+
+class Chunk(metaclass=ABCMeta):
+
+    def __init__(self, **kwargs):
+        self.span = 1
+        for k, v in kwargs.items():
+            if k in ('span', 'start', 'step'):
+                v = int(v)
+            setattr(self, k, v)
+        try:
+            if not self.chrom:
+                raise WigException("'chrom' field  is not present.")
+        except AttributeError:
+            raise WigException("'chrom' field  is not present.")
+
+        if self.span <= 0:
+            raise WigException("'{}' is not allowed as span value.".format(self.span))
+
+        self.forward = []
+        self.reverse = []
+
+    @abstractmethod
+    def is_fixed_step(self):
+        pass
+
+    @abstractmethod
+    def parse_data_line(self):
+        pass
+
+    def expand(self):
+        expanded_strands = {}
+        for sense in ('forward', 'reverse'):
+            chunk_strand = getattr(self, sense)
+            print('\n@@@@@@@@@@@@', sense, "start=", chunk_strand[0][0], "stop=", chunk_strand[-1][0], "span=",self.span)
+            expanded_strands[sense] = ExpandedStrand(chunk_strand[0][0], chunk_strand[-1][0], self.span)
+            for position, cov in chunk_strand:
+                for i in range(position, position + self.span + 1):
+                    expanded_strands[sense][i] = cov
+        return expanded_strands['forward'], expanded_strands['reverse']
+
+
+class FixedChunk(Chunk):
+
+    def __init__(self, **kwargs):
+        self.step = 1
+        super(FixedChunk, self).__init__(**kwargs)
+        if self.step == 0:
+            raise WigException
+        try:
+            self.start
+        except AttributeError:
+            raise WigException("'start' must be defined for 'fixedStep'.")
+        self._current_pos = self.start
+
+    def is_fixed_step(self):
+        return True
+
+    def parse_data_line(self, line):
+        cov = float(line)
+        if cov >= 0:
+            self.forward.append((self._current_pos, cov))
+        else:
+            self.reverse.append((self._current_pos, abs(cov)))
+        self._current_pos += self.step
+
+
+class VariableChunk(Chunk):
+
+    def is_fixed_step(self):
+        return False
+
+    def parse_data_line(self, line):
+        pos, cov = line.split()
+        pos = int(pos)
+        cov = float(cov)
+        if cov >= 0:
+            self.forward.append((pos, cov))
+        else:
+            self.reverse.append((pos, abs(cov)))
+
+
+class Strand:
+
+    def __init__(self):
+        self.coverage = []
+
+    def get_coverage(self, start, stop):
+        cov_len = len(self.coverage)
+        if 0 <= start <= cov_len:
+            if stop < cov_len:
+                return self.coverage[start: stop + 1]
+            else:
+                cov = self.coverage[start:]
+                right_fill = [0] * (stop - start - len(cov))
+                return cov + right_fill
+        elif start < 0:
+            left_fill = [0] * abs(start)
+            if stop < cov_len:
+                return left_fill + self.coverage[: stop + 1]
+            else:
+                cov = self.coverage
+                right_fill = [0] * (stop - cov_len)
+                return left_fill + cov + right_fill
+        elif start > cov_len:
+            return [0] * (stop - start)
+
+
+class Chromosome:
+
+    def __init__(self, name):
+        self.name = name
+        self._forward = Strand()
+        self._reverse = Strand()
+
+    @property
+    def forward(self):
+        return self.forward
+
+    @property
+    def reverse(self):
+        return self.reverse
+
+    def __iadd__(self, chunk):
+        if not isinstance(chunk, Chunk):
+            raise TypeError("can add only Chunk objects, provide: {}".chunk.__class__.__name__)
+        forward, reverse = chunk.expand()
+        for sense in ('forward', 'reverse'):
+            strand = getattr(self, sense)
+            # we switch from real position to zero based position
+            fill = [0] * (locals()[sense].start - 1 - len(strand))
+            strand.expand(fill)
+            strand.expand(forward)
+
+
+class Wig:
+
+    def __init__(self):
+        self._chromosomes = None
+        self.type = None
+        self.infos = {}
+
+
+    def get_chromosome(self, name, default):
+        return self._chromosomes.get(name, default)
+
+
+    def __getitem__(self, name):
+        return self._chromosomes[name]
+
+    def __setitem__(self, name, chrom):
+        self._chromosomes[name] = chrom
+
+    def __contains__(self, name):
+        return name in self._chromosomes
+
+
+class WigParser:
+
+    def __init__(self, path):
+        self.declaration_type_pattern = re.compile('fixedStep|variableStep')
+        self.path = path
+        self.wig = Wig()
+        self._current_chunk = None
+        self._current_chrom = None
+
+
+    def parse(self):
+        with open(self.path, 'r') as wig_file:
+            for line in wig_file:
+                line = line.strip()
+                if not line or self.is_comment_line(line):
+                    continue
+                elif self.is_track_line(line):
+                    self.parse_track_line(line)
+                elif self.is_declaration_line():
+                    self.parse_track_line(line)
+                else:
+                    self._current_chunk['parse_data_line'](line)
+            # we are at the end of the file
+            # so add the last chunk to the others
+            self._current_chrom += self._current_chunk
+
+
+    def parse_track_line(self, line):
+        fields = line.re.findall('(\w+=\S+|".*"|\'.*\')', line)
+        attrs = {}
+        for attr, val in fields.split('='):
+            attrs[attr] = val
+        if 'type' not in attrs:
+            raise WigException('wiggle type is not present.')
+        else:
+            self.wig.type = attrs['type']
+            del attrs['type']
+            self.wig.infos = attrs
+
+
+    def parse_declaration_line(self, line):
+        if self._current_chunk:
+            self._current_chrom += self._current_chunk
+            self._current_chunk = None
+
+        fields = line.split()
+        type = fields[0]
+
+        kwargs = {attr: val for attr, val in [f.split('=') for f in fields[1:]]}
+
+        if type == 'fixedStep':
+            self._current_chunk = FixedChunk(kwargs)
+        else:
+            self._current_chunk = VariableChunk(kwargs)
+
+        chrom_name = self._current_chunk.chrom
+        if chrom_name in self.wig:
+            chrom = self.wig[chrom_name]
+        else:
+            self.wig[chrom_name] = Chromosome(chrom_name)
+
+        self._current_chrom = chrom
+
+
+    def parse_data_line(self, line):
+        self._current_chunk.parse_data_line(line)
+
+
+    @staticmethod
+    def is_track_line(line):
+        return line.startswith('track')
+
+
+    def is_declaration_line(self, line):
+        return bool(re.match(self.declaration_type_pattern, line))
+
+
+    def is_comment_line(self, line):
+        return line.startswith('#')
+
+
+
---
+++ tests/unit/test_wig.py
@@ -0,0 +1,207 @@
+###########################################################################
+#                                                                         #
+# This file is part of Counter RNAseq Window (craw) package.              #
+#                                                                         #
+#    Authors: Bertrand Néron                                              #
+#    Copyright © 2017  Institut Pasteur (Paris).                          #
+#    see COPYRIGHT file for details.                                      #
+#                                                                         #
+#    craw is free software: you can redistribute it and/or modify         #
+#    it under the terms of the GNU General Public License as published by #
+#    the Free Software Foundation, either version 3 of the License, or    #
+#    (at your option) any later version.                                  #
+#                                                                         #
+#    craw is distributed in the hope that it will be useful,              #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 #
+#    See the GNU General Public License for more details.                 #
+#                                                                         #
+#    You should have received a copy of the GNU General Public License    #
+#    along with craw (see COPYING file).                                  #
+#    If not, see <http://www.gnu.org/licenses/>.                          #
+#                                                                         #
+###########################################################################
+
+
+
+try:
+    from tests import CRAWTest
+except ImportError as err:
+    msg = "Cannot import craw, check your installation or your CRAW_HOME variable : {0!s}".format(err)
+    raise ImportError("Cannot import craw, check your installation or your CRAW_HOME variable : {0!s}".format(err))
+
+from craw.wig import WigException, ExpandedStrand, FixedChunk, VariableChunk, Strand, Chromosome
+
+
+class TestExpandedStrand(CRAWTest):
+
+    def test_coverage(self):
+        es = ExpandedStrand(1, 5, 3)
+        self.assertListEqual(es.coverage, [0, 0, 0, 0, 0, 0, 0])
+
+    def test_len(self):
+        es = ExpandedStrand(1, 5, 3)
+        self.assertEqual(len(es), 7)
+
+    def test_stop(self):
+        es = ExpandedStrand(1, 5, 3)
+        self.assertEqual(es.stop, 7)
+
+    def test_start(self):
+        es = ExpandedStrand(1, 5, 3)
+        self.assertEqual(es.start, 1)
+
+    def test_eq(self):
+        es1 = ExpandedStrand(1, 5, 3)
+        es2 = ExpandedStrand(1, 5, 3)
+        self.assertEqual(es1, es2)
+
+
+class TestFixedChunk(CRAWTest):
+
+    def test_is_fixed_step(self):
+        kwargs = {"chrom": "chr3", "start": "400601"}
+        fx_ch = FixedChunk(**kwargs)
+        self.assertEqual(fx_ch.chrom, kwargs["chrom"])
+        self.assertEqual(fx_ch.start, int(kwargs["start"]))
+        self.assertEqual(fx_ch.step, 1)
+        self.assertEqual(fx_ch.span, 1)
+        kwargs = {"chrom": "chr3", "start": "400601", "step": "100"}
+        fx_ch = FixedChunk(**kwargs)
+        self.assertEqual(fx_ch.chrom, kwargs["chrom"])
+        self.assertEqual(fx_ch.start, int(kwargs["start"]))
+        self.assertEqual(fx_ch.step, int(kwargs["step"]))
+        self.assertEqual(fx_ch.span, 1)
+        kwargs = {"chrom": "chr3", "start": "400601", "step": "100", "span": "5"}
+        fx_ch = FixedChunk(**kwargs)
+        self.assertEqual(fx_ch.chrom, kwargs["chrom"])
+        self.assertEqual(fx_ch.start, int(kwargs["start"]))
+        self.assertEqual(fx_ch.step, int(kwargs["step"]))
+        self.assertEqual(fx_ch.span, int(kwargs["span"]))
+
+        kwargs = {"start": "400601", "step": "100", "span": "5"}
+        with self.assertRaises(WigException) as ctx:
+            FixedChunk(**kwargs)
+        self.assertEqual(str(ctx.exception), "'chrom' field  is not present.")
+
+        kwargs = {"chrom": "chr3", "start": "400601", "step": "100", "span": "0"}
+        with self.assertRaises(WigException) as ctx:
+            FixedChunk(**kwargs)
+        self.assertEqual(str(ctx.exception), "'0' is not allowed as span value.")
+
+        kwargs = {"chrom": "chr3", "step": "100", "span": "1"}
+        with self.assertRaises(WigException) as ctx:
+            FixedChunk(**kwargs)
+        self.assertEqual(str(ctx.exception), "'start' must be defined for 'fixedStep'.")
+
+
+    def test_is_fixed_step(self):
+        kwargs = {"chrom": "chr3", "start": "400601", "step": "100", "span": "5"}
+        fx_ch = FixedChunk(**kwargs)
+        self.assertTrue(fx_ch.is_fixed_step())
+
+    def test_parse_data_line(self):
+        lines = ("11", "22", "-30", "-50")
+        kwargs = {"chrom": "chr3", "start": "10", "step": "10", "span": "2"}
+        fx_ch = FixedChunk(**kwargs)
+        for l in lines:
+            fx_ch.parse_data_line(l)
+        self.assertListEqual(fx_ch.forward, [(10, float(11)), (20, float(22))])
+        self.assertListEqual(fx_ch.reverse, [(30, float(30)), (40, float(50))])
+
+    def test_expand(self):
+        lines = ("11", "22", "-30", "-50")
+        kwargs = {"chrom": "chr3", "start": "10", "step": "10", "span": "2"}
+        fx_ch = FixedChunk(**kwargs)
+        for l in lines:
+            fx_ch.parse_data_line(l)
+
+        forward, reverse = fx_ch.expand()
+        expected_for = ExpandedStrand(10, 40, 2)
+        expected_for[10] = 11.0
+        expected_for[11] = 11.0
+        expected_for[20] = 22.0
+        expected_for[21] = 22.0
+
+        expected_rev = ExpandedStrand(10, 40, 2)
+        expected_rev[30] = 30.0
+        expected_rev[31] = 30.0
+        expected_rev[40] = 50.0
+        expected_rev[41] = 50.0
+
+        self.assertEqual(forward, expected_for)
+        self.assertEqual(reverse, expected_rev)
+
+
+class TestVariableChunk(CRAWTest):
+
+    def test_is_fixed_step(self):
+        kwargs = {"chrom": "chr3"}
+        var_ch = VariableChunk(**kwargs)
+        self.assertEqual(var_ch.chrom, kwargs["chrom"])
+        self.assertEqual(var_ch.step, 1)
+        self.assertEqual(var_ch.span, 1)
+        kwargs = {"chrom": "chr3", "step": "100"}
+        var_ch = VariableChunk(**kwargs)
+        self.assertEqual(var_ch.chrom, kwargs["chrom"])
+        self.assertEqual(var_ch.step, int(kwargs["step"]))
+        self.assertEqual(var_ch.span, 1)
+        kwargs = {"chrom": "chr3", "step": "100", "span": "5"}
+        var_ch = VariableChunk(**kwargs)
+        self.assertEqual(var_ch.chrom, kwargs["chrom"])
+        self.assertEqual(var_ch.step, int(kwargs["step"]))
+        self.assertEqual(var_ch.span, int(kwargs["span"]))
+
+        kwargs = {"span": "5"}
+        with self.assertRaises(WigException) as ctx:
+            VariableChunk(**kwargs)
+        self.assertEqual(str(ctx.exception), "'chrom' field  is not present.")
+
+        kwargs = {"chrom": "chr3", "span": "0"}
+        with self.assertRaises(WigException) as ctx:
+            VariableChunk(**kwargs)
+        self.assertEqual(str(ctx.exception), "'0' is not allowed as span value.")
+
+
+    def test_is_fixed_step(self):
+        kwargs = {"chrom": "chr3", "span": "5"}
+        var_ch = VariableChunk(**kwargs)
+        self.assertFalse(var_ch.is_fixed_step())
+
+    def test_parse_data_line(self):
+        lines = ("10 11", "20 22", "30 -30", "40 -50")
+        kwargs = {"chrom": "chr3", "span": "2"}
+        var_ch = VariableChunk(**kwargs)
+        for l in lines:
+            var_ch.parse_data_line(l)
+        self.assertListEqual(var_ch.forward, [(10, float(11)), (20, float(22))])
+        self.assertListEqual(var_ch.reverse, [(30, float(30)), (40, float(50))])
+
+
+class TestStrand(CRAWTest):
+
+    def test_get_coverage(self):
+        st = Strand()
+        st.coverage = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertListEqual(st.get_coverage(1, 4), [1, 2, 3, 4])
+        self.assertListEqual(st.get_coverage(-1, 4), [0, 0, 1, 2, 3, 4])
+        self.assertListEqual(st.get_coverage(5, 12), [5, 6, 7, 8, 9, 0, 0])
+        self.assertListEqual(st.get_coverage(-2, 12), [0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0])
+
+class TestChromosome(CRAWTest):
+
+    def test_Chromosome(self):
+        ch_name = 'ChrII'
+        ch = Chromosome(ch_name)
+        self.assertEqual(ch.name, ch_name)
+
+    def test_iadd(self):
+        ch_name = 'ChrII'
+        ch = Chromosome(ch_name)
+
+        # kwargs = {"chrom": ch_name, "start": "10"}
+        # fx_ck = FixedChunk(**kwargs)
+        # lines = ("11", "22", "-30", "-50")
+        # for l in lines:
+        #     fx_ck.parse_data_line(l)
+        # ch += fx_ck
