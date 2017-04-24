0f056d3a879f84d214d14723fff51bd19ed9cea4
Bertrand Néron <bneron@pasteur.fr>
2017-04-24 16:07:59 +0200

    test exend memeory on git lab runner

--- tests/unit/test_wig.py
+++ tests/unit/test_wig.py
@@ -259,6 +259,8 @@ class TestChromosome(CRAWTest):
         with self.assertRaises(MemoryError) as ctx:
             # 1 billion
             ch[1000 * 1000 * 1000 * 1000] = 10
+            print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print(str(ctx.exception))
         self.assertTrue(str(ctx.exception).startswith("Not enough memory to extend chromosome"))
 
 
